1d01519bbd7cc89aef3671bff792894cc4152e80
NataliaProkop <natalia.prokop1@gmail.com>
2017-11-19 21:59:46 +0100

    Added swagger and docker file

--- src/main/kotlin/com/jalgoarena/JAlgoArenaEurekaServer.kt
+++ src/main/kotlin/com/jalgoarena/JAlgoArenaEurekaServer.kt
@@ -4,12 +4,28 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+import org.springframework.context.annotation.Bean
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableHystrixDashboard
+@EnableSwagger2
 open class JAlgoArenaEurekaServer
 
 fun main(args: Array<String>) {
     SpringApplication.run(JAlgoArenaEurekaServer::class.java, *args)
 }
+
+@Bean
+fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
+        .select() 
+        .apis(RequestHandlerSelectors.basePackage("com.jalgoarena.web"))
+        .paths(PathSelectors.any())                          
+        .build()
